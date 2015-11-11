778a3d8349b1f803a395c81a80175cf2cefb4d9e
Ryoji <kuniko192002@gmail.com>
2015-11-11 21:16:32 +0900

    add main class

---
+++ src/main/java/net/yosshio0426/testbuild/Main.java
@@ -0,0 +1,15 @@
+package net.yosshio0426.testbuild;
+
+/**
+ * EntryPoint.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().helloWorld();
+    }
+
+    public void helloWorld() {
+        System.out.println("hello world");
+    }
+}
